8c39cea817478e93095ef14ac8c04b1cf5c02fc9
anthonydevelops <anthonydevelops@gmail.com>
2018-12-12 21:48:29 -0800

    Captured all job links

--- jobs.py
+++ jobs.py
@@ -8,6 +8,7 @@ links = [
 ]
 
 jobs = []
+urls = []
 
 
 class Job:
@@ -23,7 +24,17 @@ for link in links:
     req = requests.get(link)
     soup = BeautifulSoup(req.text, 'html.parser')
 
-    job_list = soup.find_all('td', class_='smallwording')
+    # Gets the excerpt of the job posting
+    job_list = soup.find_all('td', class_="smallwording")
+
+    # Gets all the url links to job postings
+    href_tags = soup.find_all('a', href=re.compile('action=displayER'))
+    for href_tag in href_tags:
+        urls.append(
+            'http://www.careercenter.ucsc.edu/ers/erspub/' + href_tag.get('href'))
+
+    # Cycles through job excerpt to parse info in chunks
     for job in job_list:
-        title = job.contents[0]
-        print(title)
+        listing = job.contents[0]
+
+print(urls)
